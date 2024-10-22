c331cecd3aba6947a5af6c4109bc3b93cb0d56e4
Paul Molodowitch <pmolodowitch@nvidia.com>
2024-10-22 15:22:37 -0700

    angleScale_fromulas.py: add bimodal, GraphOptions

--- pxr/usd/usdLux/iesAngleScale/angleScale_formulas.py
+++ pxr/usd/usdLux/iesAngleScale/angleScale_formulas.py
@@ -1,17 +1,23 @@
 # execfile(r"C:\src\NVIDIA\usd-ci\USD\pxr\usd\usdLux\iesAngleScale\angleScale_formulas.py")
+from attrs import frozen
+import dataclasses
 import inspect
 import math
 import os
 
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d
+import numpy
+from numpy import angle
 import spb
 import sympy
+
+from typing import Optional, Tuple
 
 # from sympy import *
 from spb import plot, plot3d
 
-INTERACTIVE_VIEW = False
+INTERACTIVE_VIEW = True
 
 
 THIS_FILE = os.path.abspath(inspect.getsourcefile(lambda: None) or __file__)
@@ -25,6 +31,78 @@ def Clamp(x, min, max):
     return sympy.Min(sympy.Max(x, min), max)
 
 
+theta, angleScale = sympy.symbols("theta angleScale")
+
+# angle_units = "radians"
+ANGLE_UNITS = "degrees"
+
+if ANGLE_UNITS == "radians":
+    THETA_MAX = sympy.pi
+elif ANGLE_UNITS == "degrees":
+    THETA_MAX = 180
+
+# in latex format
+THETA_INPUT_LABEL = r"$\theta_{light}$"
+THETA_OUTPUT_LABEL = r"$\theta_{ies}$"
+THETA_INPUT_LABEL = f"{THETA_INPUT_LABEL} ({ANGLE_UNITS})"
+THETA_OUTPUT_LABEL = f"{THETA_OUTPUT_LABEL} ({ANGLE_UNITS})"
+
+THETA_MIN_MAX = (0, THETA_MAX)
+ANGLESCALE_MIN_MAX = (-1, 1)
+
+@dataclasses.dataclass(frozen=True)
+class GraphOptions:
+    theta_min_max: Tuple[float, float] = THETA_MIN_MAX
+    angleScale_min_max: Tuple[float, float] = ANGLESCALE_MIN_MAX
+    axis_size_scales: Tuple[float, float, float] = (1, 1, 1)
+    view_angles: Optional[Tuple[float, float, float]] = None
+
+    def set_on_graph(self, graph):
+        return self.set_on_axes(graph.ax)
+
+    def set_on_axes(self, axes):
+        if not "theta" in axes.get_xlabel():
+            raise ValueError("expected x-axis to be theta")
+
+        axes.set_xlabel(THETA_INPUT_LABEL)
+        axes.set_xlim(*self.theta_min_max)
+        if hasattr(axes, "set_zlim"):
+            axes.set_ylim(*self.angleScale_min_max)
+            axes.set_zlim(*self.theta_min_max)
+            axes.set_zlabel(THETA_OUTPUT_LABEL)
+        else:
+            axes.set_ylim(*self.theta_min_max)
+            axes.set_ylabel(THETA_OUTPUT_LABEL)
+
+        if self.axis_size_scales != (1, 1, 1):
+            set_axis_size_scales(axes, self.axis_size_scales)
+
+    def theta_lim(self):
+        return (theta,) + self.theta_min_max
+
+    def angleScale_lim(self):
+        return (angleScale,) + self.angleScale_min_max
+
+    def make_graph(self, function, title):
+        num_symbols = len(function.free_symbols)
+        if num_symbols == 1:
+            is3d = False
+        elif num_symbols == 2:
+            is3d = True
+        else:
+            raise ValueError(num_symbols)
+
+        if is3d:
+            graph = spb.plot3d(function, self.theta_lim(), self.angleScale_lim(), show=False, title=title)
+        else:
+            graph = spb.plot(function, self.theta_lim(), show=False, title=title)
+
+        self.set_on_graph(graph)
+        return graph
+
+
+DEFAULT_GRAPH = GraphOptions()
+
 def save_graph(graph, filename, dpi=300):
     ext = os.path.splitext(filename)[-1].lstrip(".")
     if not ext or ext.isdigit():
@@ -35,61 +113,38 @@ def save_graph(graph, filename, dpi=300):
     # "graph.process_series()", but that seemed to make a second copy of the
     # graph, that didn't respect the zlim
     axes = graph.ax
-
-    set_axis_limits(axes)
     graph.fig.savefig(filepath, dpi=dpi)
 
 
-theta, angleScale = sympy.symbols("theta angleScale")
+def set_axis_size_scales(axes, axis_size_scales: Tuple[float, float, float]):
+    from mpl_toolkits.mplot3d.axes3d import Axes3D
+
+    max_scale = max(axis_size_scales)
+    diagonal = numpy.array(axis_size_scales) / max_scale
+    diagonal = numpy.insert(diagonal, 3, 1.0)
+    scale = numpy.diag(diagonal)
+
+    def scaled_projection():
+        return numpy.dot(Axes3D.get_proj(axes), scale)
+
+    axes.get_proj = scaled_projection
 
 
-# angle_units = "radians"
-angle_units = "degrees"
-
-if angle_units == "radians":
-    theta_max = sympy.pi
-elif angle_units == "degrees":
-    theta_max = 180
-
-# in latex format
-theta_input_label = r"$\theta_{light}$"
-theta_output_label = r"$\theta_{ies}$"
-theta_input_label = f"{theta_input_label} ({angle_units})"
-theta_output_label = f"{theta_output_label} ({angle_units})"
-
-theta_lim = (theta, 0, theta_max)
-angleScale_lim = (angleScale, -1, 1)
-
-
-def set_axis_limits(axes):
-    if not "theta" in axes.get_xlabel():
-        raise ValueError("expected x-axis to be theta")
-    axes.set_xlabel(theta_input_label)
-    axes.set_xlim(*theta_lim[1:])
-    if hasattr(axes, "set_zlim"):
-        axes.set_ylim(*angleScale_lim[1:])
-        axes.set_zlim(*theta_lim[1:])
-        axes.set_zlabel(theta_output_label)
-    else:
-        axes.set_ylim(*theta_lim[1:])
-        axes.set_ylabel(theta_output_label)
-
-
-def save_graph_slices(function, title, filename):
+def save_graph_slices(function, title, filename, graph_options=DEFAULT_GRAPH):
     for i in range(-100, 101, 25):
         angleScale_val = i / 100
         angleScale_str = f"{angleScale_val:+.02f}"
         title_i = f"{title} (angleScale = {angleScale_str})"
         filename_i = f"{filename}_{angleScale_str}"
         slice_func = function.subs(angleScale, angleScale_val)
-        graph = spb.plot(slice_func, theta_lim, show=False, title=title_i)
+        graph = graph_options.make_graph(slice_func, title=title_i)
         if any(
             isinstance(x, sympy.core.numbers.ComplexInfinity)
             for x in slice_func.atoms()
         ):
             del graph.series[0]
             axes = graph.ax
-            set_axis_limits(axes)
+            graph_options.set_on_axes(axes)
             x_center = sum(axes.get_xlim()) / 2
             y_center = sum(axes.get_ylim()) / 2
             axes.text(
@@ -106,8 +161,8 @@ def save_graph_slices(function, title, filename):
         plt.close(graph.fig)
 
 
-def plot3d_and_save(function, title, slices=False):
-    graph = spb.plot3d(function, theta_lim, angleScale_lim, show=False, title=title)
+def plot3d_and_save(function, title, slices=False, graph_options=DEFAULT_GRAPH):
+    graph = graph_options.make_graph(function, title=title)
     filename = f"ies_angleScale_{title}"
     filename = filename.replace(" - ", "-")
     filename = filename.replace("/", "over")
@@ -117,7 +172,7 @@ def plot3d_and_save(function, title, slices=False):
 
     save_graph(graph, filename)
     if slices:
-        save_graph_slices(function, title, filename)
+        save_graph_slices(function, title, filename, graph_options=graph_options)
     return graph
 
 
@@ -130,39 +185,56 @@ karma = sympy.Piecewise(
     (karma_pos, angleScale < 1),
     (0, True),
 )
-karma_clamp = Clamp(karma, 0, theta_max)
+karma_clamp = Clamp(karma, 0, THETA_MAX)
 
 karma_pos_clamp = Clamp(
     sympy.Piecewise((0, angleScale < -1), (karma_pos, angleScale < 1), (0, True)),
     0,
-    theta_max,
+    THETA_MAX,
 )
 
 
 # karma_pos_graph = plot3d_and_save(karma_pos, "Karma (positive)")
 # karma_neg_graph = plot3d_and_save(karma_neg, "Karma (negative)")
 
-karma_graph = plot3d_and_save(karma, "Karma (unclamped)")
+# karma_graph = plot3d_and_save(karma, "Karma (unclamped)")
 
-karma_clamp_graph = plot3d_and_save(karma_clamp, "Karma (clamped)", slices=True)
+# karma_clamp_graph = plot3d_and_save(karma_clamp, "Karma (clamped)", slices=True)
 # save_graph_slices(karma_clamp, "ies_angleScale_karma_clamped")
 
-karma_pos_clamp_graph = plot3d_and_save(
-    karma_pos_clamp, "Karma - theta / (1-angleScale) only (clamped)"
-)
+# karma_pos_clamp_graph = plot3d_and_save(
+#     karma_pos_clamp, "Karma - theta / (1-angleScale) only (clamped)"
+# )
 
 profile_scale = 1 + angleScale
-rman = ((theta - theta_max) / profile_scale) + theta_max
-rman_clamp = Clamp(sympy.Piecewise((rman, angleScale > -1), (0, True)), 0, theta_max)
+rman = ((theta - THETA_MAX) / profile_scale) + THETA_MAX
+rman_clamp = Clamp(sympy.Piecewise((rman, angleScale > -1), (0, True)), 0, THETA_MAX)
 
-rman_graph = plot3d_and_save(rman, "Renderman (unclamped)")
+# rman_graph = plot3d_and_save(rman, "Renderman (unclamped)")
 
-rman_clamp_graph = plot3d_and_save(rman_clamp, "Renderman (clamped)", slices=True)
+# rman_clamp_graph = plot3d_and_save(rman_clamp, "Renderman (clamped)", slices=True)
+
+
+# if angleScale > 0, scale origin is at bottom (vangle=0)
+# if angleScale == 0, no change
+# if angleScale < 0, scale origin is at top (vangle=180)
+
+bimodal_pos = theta / angleScale
+bimodal_neg = ((theta - THETA_MAX) / -angleScale) + THETA_MAX
+bimodal_neg_clamp = Clamp(bimodal_neg, 0, THETA_MAX)
+# bimodal_neg_clamp_graph = plot3d_and_save(bimodal_neg_clamp, "Bimodal Neg (Clamped)", slices=False, angleScale_min_max=(-1, 0))
+
+bimodal = sympy.Piecewise((bimodal_pos, angleScale > 0), (bimodal_neg, angleScale < 0), (theta, True))
+bimodal_clamp = Clamp(bimodal, 0, THETA_MAX)
+
+bimodal_options = GraphOptions(angleScale_min_max=(-2, 2), axis_size_scales=(1, 2, 1))
+bimodal_clamp_graph = plot3d_and_save(bimodal_clamp, "Bimodal (clamped)", slices=False, graph_options=bimodal_options)
 
 if INTERACTIVE_VIEW:
     plt.show()
 else:
-    plt.close("all")
+    # plt.close("all")
+    pass
 
 # from sympy.solvers import solve
 

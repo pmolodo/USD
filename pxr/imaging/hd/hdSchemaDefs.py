2f668a6044385c7b0e1dc2b73838c6a2b0b1b1a9
Tom <tcauchois@pixar.com>
2024-11-22 15:21:45 -0800

    Add exposure parameters to hydra camera schema

--- pxr/imaging/hd/hdSchemaDefs.py
+++ pxr/imaging/hd/hdSchemaDefs.py
@@ -1,25 +1,8 @@
 #
 # Copyright 2023 Pixar
 #
-# Licensed under the Apache License, Version 2.0 (the "Apache License")
-# with the following modification; you may not use this file except in
-# compliance with the Apache License and the following modification to it:
-# Section 6. Trademarks. is deleted and replaced with:
-#
-# 6. Trademarks. This License does not grant permission to use the trade
-#    names, trademarks, service marks, or product names of the Licensor
-#    and its affiliates, except as required to comply with Section 4(c) of
-#    the License and to reproduce the content of the NOTICE file.
-#
-# You may obtain a copy of the Apache License at
-#
-#     http:#www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the Apache License with the above modification is
-# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied. See the Apache License for the specific
-# language governing permissions and limitations under the Apache License.
+# Licensed under the terms set forth in the LICENSE.txt file available at
+# https://openusd.org/license.
 #
 [
     dict(
@@ -905,7 +888,12 @@
             ('focusDistance', T_FLOAT, {}),
             ('shutterOpen', T_DOUBLE, dict(ADD_LOCATOR = True)),
             ('shutterClose', T_DOUBLE, dict(ADD_LOCATOR = True)),
-            ('exposure', T_FLOAT, {}),
+            ('exposure', T_FLOAT, dict(ADD_LOCATOR = True)),
+            ('exposureTime', T_FLOAT, dict(ADD_LOCATOR = True)),
+            ('exposureIso', T_FLOAT, dict(ADD_LOCATOR = True)),
+            ('exposureFStop', T_FLOAT, dict(ADD_LOCATOR = True)),
+            ('exposureResponsivity', T_FLOAT, dict(ADD_LOCATOR = True)),
+            ('exposureCompensation', T_FLOAT, dict(ADD_LOCATOR = True)),
             ('focusOn', T_BOOL, {}),
             ('dofAspect', T_FLOAT, {}),
             ('splitDiopter', 'HdSplitDiopterSchema', {}),
